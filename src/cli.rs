2c45198b09982fa78ec3b55931e101107d675d3a
Michał Kalbarczyk <fazibear@gmail.com>
2025-05-10 17:02:09 +0200

    switch to rb

--- src/cli.rs
+++ src/cli.rs
@@ -20,31 +20,8 @@ fn main() -> Result<()> {
     let path = std::path::Path::new(&filename);
     let data = std::fs::read(path)?;
 
-    let buffer = HeapRb::<i16>::new(44100 * 2);
-    let (mut prod, mut cons) = buffer.split();
-
-    let sound = Sound::new()?;
-
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-    let dev_rn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-        if data.len() / 2 > cons.occupied_len() {
-            println!("ups!");
-            return;
-        }
-        for samples in data.chunks_mut(2) {
-            let val = cons.try_pop().unwrap();
-            let sample_val = i16::from_sample(val).to_sample::<f32>();
-            for sample in samples {
-                *sample = sample_val;
-            }
-        }
-    };
-
-    let stream = sound
-        .device
-        .build_output_stream(&sound.config, dev_rn, err_fn, None)?;
-
-    let _ = stream.play();
+    let mut sound = Sound::new()?;
+    let _ = sound.stream.play();
 
     let mut player = Player::new(&data)?;
     player.init();
@@ -67,14 +44,8 @@ fn main() -> Result<()> {
                     .memory
                     .borrow_mut()
                     .sid_sample(delta, &mut buffer[..], 1);
-            prod.push_slice(&buffer[..samples]);
+            sound.write_blocking(&buffer[..samples]);
             delta = next_delta;
-        }
-
-        if let ((BUFFER_SIZE..), Some(time)) =
-            (prod.occupied_len(), player.speed.checked_sub(now.elapsed()))
-        {
-            thread::sleep(time)
         }
     }
 
--- src/sound.rs
+++ src/sound.rs
@@ -2,30 +2,77 @@ use anyhow::Result;
 
 use cpal::{
     traits::{DeviceTrait, HostTrait},
-    Device, StreamConfig,
+    Device, Sample, Stream, StreamConfig,
 };
+
+use rb::{RbConsumer, RbProducer, SpscRb, RB};
 
 pub struct Sound {
     pub device: Device,
     pub config: StreamConfig,
+    pub stream: Stream,
+    pub buffer: SpscRb<i16>,
 }
 
 impl Sound {
     pub fn new() -> Result<Self> {
         let host = cpal::default_host();
-        let device = host.default_output_device().expect("no output device available");
+        let device = host
+            .default_output_device()
+            .expect("no output device available");
         let supported_config = device.default_output_config()?;
         let config = supported_config.into();
-        
-        Ok(Self { device, config })
+
+        let buffer = SpscRb::new(1024);
+
+        let consumer = buffer.consumer();
+
+        let stream = device
+            .build_output_stream(
+                &config,
+                move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                    let mut tmp: Vec<i16> = vec![0; data.len() / 2];
+                    let _readed = consumer.read(&mut tmp[..]).unwrap_or(0);
+                    //  let new = tmp.iter().map(|&s| f32::from_sample(s)).collect::<Vec<_>>();
+
+                    let new = tmp
+                        .iter()
+                        .flat_map(|&s| {
+                            let v = f32::from_sample(s);
+                            vec![v, v]
+                        })
+                        .collect::<Vec<_>>();
+
+                    data.copy_from_slice(&new);
+                },
+                move |err| {
+                    dbg!("audio output error: {}", err);
+                },
+                None,
+            )
+            .expect("create a stream");
+
+        Ok(Self {
+            device,
+            config,
+            stream,
+            buffer,
+        })
     }
-    
+
     pub fn info(&self) -> Result<()> {
         eprintln!("Output device: {}", self.device.name()?);
-        eprintln!("Supported stream config: {:?}", self.device.default_output_config()?);
+        eprintln!(
+            "Supported stream config: {:?}",
+            self.device.default_output_config()?
+        );
         eprintln!("Stream config: {:?}", self.config);
 
         Ok(())
+    }
+
+    pub fn write_blocking(&mut self, data: &[i16]) {
+        self.buffer.producer().write_blocking(data);
     }
 
     // pub fn stream(&self, dev_fn: impl FnMut(&mut [f32], &OutputCallbackInfo)) -> Result<(), Box<dyn std::error::Error>> {
